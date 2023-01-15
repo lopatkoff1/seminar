c260566c5a478563e2dcdd6f01d3368ad4b6c70e
Pavel <kgk174@bk.ru>
2023-01-15 12:55:26 +0300

    ToLower

---
+++ Example006_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользоватиля:");
+string username = Console.ReadLine();
+
+if(username.ToLower()== "паша")
+{
+    Console.WriteLine("Ураа,это Паша");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
